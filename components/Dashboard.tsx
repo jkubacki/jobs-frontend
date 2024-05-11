5d1fb1ce5f162139d86e01f9317026c91513cd09
Jakub Kubacki <kubacki.jk@gmail.com>
2024-05-11 22:05:49 +0200

    Change icon

--- components/Dashboard.tsx
+++ components/Dashboard.tsx
@@ -6,7 +6,6 @@ import {
   LineChart,
   ListFilter,
   Package,
-  Package2,
   PanelLeft,
   PlusCircle,
   Search,
@@ -67,7 +66,7 @@ export function Dashboard() {
             href="#"
             className="group flex h-9 w-9 shrink-0 items-center justify-center gap-2 rounded-full bg-primary text-lg font-semibold text-primary-foreground md:h-8 md:w-8 md:text-base"
           >
-            <Package2 className="h-4 w-4 transition-all group-hover:scale-110" />
+            <div className="text-xs transition-all group-hover:scale-110">JBS</div>
             <span className="sr-only">Acme Inc</span>
           </Link>
           <Tooltip>
@@ -147,8 +146,8 @@ export function Dashboard() {
                   href="#"
                   className="group flex h-10 w-10 shrink-0 items-center justify-center gap-2 rounded-full bg-primary text-lg font-semibold text-primary-foreground md:text-base"
                 >
-                  <Package2 className="h-5 w-5 transition-all group-hover:scale-110" />
-                  <span className="sr-only">Acme Inc</span>
+                  <div className="text-xs transition-all group-hover:scale-110">JBS</div>
+                  <span className="sr-only">Jobs</span>
                 </Link>
                 <Link
                   href="#"
