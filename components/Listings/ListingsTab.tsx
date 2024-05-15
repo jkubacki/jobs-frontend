7067a8c0084bfce33016f435188d14e8b72e1cf3
Jakub Kubacki <kubacki.jk@gmail.com>
2024-05-15 20:50:01 +0200

    Remove dropdown filter

--- components/Listings/ListingsTab.tsx
+++ components/Listings/ListingsTab.tsx
@@ -1,17 +1,6 @@
 'use client'
 
-import { ListFilter } from 'lucide-react'
-
-import {
-  DropdownMenu,
-  DropdownMenuTrigger,
-  DropdownMenuContent,
-  DropdownMenuLabel,
-  DropdownMenuSeparator,
-  DropdownMenuCheckboxItem,
-} from '@/components/ui/dropdown-menu'
 import { CreateListingDialog } from '@/components/CreateListingDialog/CreateListingDialog'
-import { Button } from '@/components/ui/button'
 import { RadioTabs } from '@/components/RadioTabs/RadioTabs'
 import { useAppDispatch, useAppSelector } from '@/lib/hooks'
 import { ListingsActions, ListingsState } from '@/lib/listings/listingsSlice'
@@ -40,21 +29,6 @@ export function ListingsTab() {
       <div className="flex items-center">
         <RadioTabs tabs={tabs} tabClick={tabClick} selected={remoteFilter} />
         <div className="ml-auto flex items-center gap-2">
-          <DropdownMenu>
-            <DropdownMenuTrigger asChild>
-              <Button variant="outline" size="sm" className="h-8 gap-1">
-                <ListFilter className="h-3.5 w-3.5" />
-                <span className="sr-only sm:not-sr-only sm:whitespace-nowrap">Filter</span>
-              </Button>
-            </DropdownMenuTrigger>
-            <DropdownMenuContent align="end">
-              <DropdownMenuLabel>Filter by</DropdownMenuLabel>
-              <DropdownMenuSeparator />
-              <DropdownMenuCheckboxItem checked>Active</DropdownMenuCheckboxItem>
-              <DropdownMenuCheckboxItem>Draft</DropdownMenuCheckboxItem>
-              <DropdownMenuCheckboxItem>Archived</DropdownMenuCheckboxItem>
-            </DropdownMenuContent>
-          </DropdownMenu>
           <CreateListingDialog />
         </div>
       </div>
