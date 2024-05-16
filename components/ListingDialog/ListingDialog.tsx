d6e4a87a1774b1710be05328a07f44591393fceb
Jakub Kubacki <kubacki.jk@gmail.com>
2024-05-16 15:08:32 +0200

    Custom onOpenChange and optional dialog trigger

--- components/ListingDialog/ListingDialog.tsx
+++ components/ListingDialog/ListingDialog.tsx
@@ -1,15 +1,11 @@
 'use client'
 
-import { PlusCircle } from 'lucide-react'
-
-import { Button } from '@/components/ui/button'
 import {
   Dialog,
   DialogContent,
   DialogDescription,
   DialogHeader,
   DialogTitle,
-  DialogTrigger,
 } from '@/components/ui/dialog'
 import { ListingForm } from '@/components/ListingDialog/ListingForm'
 import { useAppDispatch } from '@/lib/hooks'
@@ -18,27 +14,21 @@ import { ListingsActions } from '@/lib/listings/listingsSlice'
 export function ListingDialog({
   title,
   action,
+  onOpenChange,
   open,
+  children,
 }: {
   title: string
   action: typeof ListingsActions.create | typeof ListingsActions.update
+  onOpenChange: (open: boolean) => void
   open: boolean
+  children?: React.ReactNode
 }) {
   const dispatch = useAppDispatch()
 
   return (
-    <Dialog
-      open={open}
-      onOpenChange={(creatingFormOpen: boolean) => {
-        dispatch(ListingsActions.setCreatingFormOpen({ creatingFormOpen }))
-      }}
-    >
-      <DialogTrigger asChild>
-        <Button size="sm" className="h-8 gap-1">
-          <PlusCircle className="h-3.5 w-3.5" />
-          <span className="sr-only sm:not-sr-only sm:whitespace-nowrap">{title}</span>
-        </Button>
-      </DialogTrigger>
+    <Dialog open={open} onOpenChange={onOpenChange}>
+      {children}
       <DialogContent className="overflow-y-scroll max-h-[95%] sm:max-w-screen-xs md:max-w-screen-sm lg:max-w-screen-md">
         <DialogHeader>
           <DialogTitle>{title}</DialogTitle>
--- components/Listings/ListingsTab.tsx
+++ components/Listings/ListingsTab.tsx
@@ -1,16 +1,21 @@
 'use client'
 
+import { PlusCircle } from 'lucide-react'
+
+import { DialogTrigger } from '@/components/ui/dialog'
 import { ListingDialog } from '@/components/ListingDialog/ListingDialog'
 import { RadioTabs } from '@/components/RadioTabs/RadioTabs'
 import { useAppDispatch, useAppSelector } from '@/lib/hooks'
 import { ListingsActions, ListingsState } from '@/lib/listings/listingsSlice'
 import { ListingsTable } from '@/components/Listings/ListingsTable'
 import { ListingsSelectors } from '@/lib/listings/ListingsSelectors'
+import { Button } from '@/components/ui/button'
 
 export function ListingsTab() {
   const dispatch = useAppDispatch()
   const remoteFilter = useAppSelector(ListingsSelectors.remoteFilter)
   const creatingFormOpen = useAppSelector(ListingsSelectors.creatingFormOpen)
+  const updatingFormOpen = useAppSelector(ListingsSelectors.updatingFormOpen)
 
   const tabClick = (remoteFilter: ListingsState['remoteFilter']) => {
     dispatch(ListingsActions.setRemoteFilter({ remoteFilter }))
@@ -33,7 +38,25 @@ export function ListingsTab() {
           <ListingDialog
             title="Add Listing"
             action={ListingsActions.create}
+            onOpenChange={(open: boolean) => {
+              dispatch(ListingsActions.setCreatingFormOpen({ creatingFormOpen: open }))
+            }}
             open={creatingFormOpen}
+          >
+            <DialogTrigger asChild>
+              <Button size="sm" className="h-8 gap-1">
+                <PlusCircle className="h-3.5 w-3.5" />
+                <span className="sr-only sm:not-sr-only sm:whitespace-nowrap">Add Listing</span>
+              </Button>
+            </DialogTrigger>
+          </ListingDialog>
+          <ListingDialog
+            title="Edit Listing"
+            action={ListingsActions.update}
+            open={updatingFormOpen}
+            onOpenChange={(open: boolean) => {
+              if (!open) dispatch(ListingsActions.setEdited({ listing: null }))
+            }}
           />
         </div>
       </div>
