eaeca9c9303b384fe13925dedca0220ae697f76b
Jakub Kubacki <kubacki.jk@gmail.com>
2024-05-17 16:51:48 +0200

    Extract badge colors

---
+++ components/Listings/ListingCard/Badges/BadgeColors.ts
@@ -0,0 +1,5 @@
+export const BadgeColors = {
+  positive: 'bg-green-600',
+  negative: 'bg-red-400',
+  neutral: 'bg-orange-400',
+}
--- components/Listings/ListingCard/Badges/RemoteBadge.tsx
+++ components/Listings/ListingCard/Badges/RemoteBadge.tsx
@@ -1,7 +1,8 @@
+import { BadgeColors } from '@/components/Listings/ListingCard/Badges/BadgeColors'
 import { Badge } from '@/components/ui/badge'
 
 export function RemoteBadge({ remote }: { remote: boolean }) {
-  const badgeColor = remote ? 'bg-green-600' : 'bg-rose-400'
+  const badgeColor = remote ? BadgeColors.positive : BadgeColors.negative
 
   return <Badge className={badgeColor}>{remote ? 'Remote' : 'Not Remote'}</Badge>
 }
