f20f1ff1c7282c61f46f761de6179398f4385c39
Jakub Kubacki <kubacki.jk@gmail.com>
2024-05-17 15:04:58 +0200

    Add all listing info to card

--- components/Listings/ListingCard/ListingCard.tsx
+++ components/Listings/ListingCard/ListingCard.tsx
@@ -1,3 +1,14 @@
+import { MoreHorizontal } from 'lucide-react'
+
+import {
+  DropdownMenu,
+  DropdownMenuTrigger,
+  DropdownMenuContent,
+  DropdownMenuLabel,
+} from '@/components/ui/dropdown-menu'
+import { DeleteListingDropdownMenuItem } from '@/components/Listings/ListingsTable/ListingTableRow/DropdownMenuItems/DeleteListingDropdownMenuItem'
+import { EditListingDropdownMenuItem } from '@/components/Listings/ListingsTable/ListingTableRow/DropdownMenuItems/EditListingDropdownMenuItem'
+import { Button } from '@/components/ui/button'
 import {
   Card,
   CardContent,
@@ -12,14 +23,37 @@ export function ListingCard({ listing }: { listing: Listing }) {
   return (
     <Card>
       <CardHeader>
-        <CardTitle>Card Title</CardTitle>
-        <CardDescription>Card Description</CardDescription>
+        <CardTitle className="text-lg">{listing.title}</CardTitle>
+        <div className="text-md">{listing.company}</div>
+        <CardDescription>{listing.product}</CardDescription>
       </CardHeader>
       <CardContent>
-        <p>Card Content</p>
+        <div>{listing.url}</div>
+        <div>{listing.compensation}</div>
+        {listing.pto && <div>PTO {listing.pto}</div>}
+        <div>{listing.description}</div>
+        <div>{listing.notes}</div>
+        <div>{listing.based_in}</div>
+        <div>{listing.timezones}</div>
+        <div>{listing.stack}</div>
+        <div>{listing.preference}%</div>
       </CardContent>
       <CardFooter>
-        <p>Card Footer</p>
+        {listing.glassdoor_rating && <span>({listing.glassdoor_rating / 10})</span>}{' '}
+        {listing.remote ? 'Remote' : 'Not Remote'}
+        <DropdownMenu>
+          <DropdownMenuTrigger asChild>
+            <Button aria-haspopup="true" size="icon" variant="ghost">
+              <MoreHorizontal className="h-4 w-4" />
+              <span className="sr-only">Toggle menu</span>
+            </Button>
+          </DropdownMenuTrigger>
+          <DropdownMenuContent align="end">
+            <DropdownMenuLabel>Actions</DropdownMenuLabel>
+            <EditListingDropdownMenuItem listing={listing} />
+            <DeleteListingDropdownMenuItem listing={listing} />
+          </DropdownMenuContent>
+        </DropdownMenu>
       </CardFooter>
     </Card>
   )
