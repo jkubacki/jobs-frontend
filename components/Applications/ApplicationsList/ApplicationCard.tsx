c2f6136b7c73d51aaf2135e117dacf6d66bca141
Jakub Kubacki <kubacki.jk@gmail.com>
2024-05-18 15:29:53 +0200

    Use ListingBadge

--- components/Applications/ApplicationsList/ApplicationCard.tsx
+++ components/Applications/ApplicationsList/ApplicationCard.tsx
@@ -1,20 +1,23 @@
-import { FileText } from 'lucide-react'
-
 import { Application } from '@/lib/applications/types/Application'
 import { Card } from '@/components/ui/card'
 import { BadgeColors } from '@/components/Listings/ListingCard/Badges/BadgeColors'
 import { PreferenceBadge } from '@/components/Listings/ListingCard/Badges/PreferenceBadge'
+import { ListingBadge } from '@/components/Listings/ListingCard/Badges/ListingBadge'
 
 export function ApplicationCard({ application }: { application: Application }) {
   const cvColor = application.cv ? 'green-600' : BadgeColors.negative
   return (
     <Card className="flex flex-col items-center gap-2 font-normal p-3 text-sm" title="Application">
       <div className="flex gap-2 justify-between w-full">
-        {application.applied_at}
+        <ListingBadge title="Applied at">{application.applied_at}</ListingBadge>
         <div className="flex gap-2">
           <div className="flex items-center gap-2">
-            <FileText className={`h-4 w-4 ${cvColor}`} />
-            {application.cv ? 'CV sent' : 'CV not sent'}
+            <ListingBadge
+              title="CV sent?"
+              color={application.cv ? BadgeColors.positive : BadgeColors.neutral}
+            >
+              {application.cv ? 'CV sent' : 'CV not sent'}
+            </ListingBadge>
           </div>
           <PreferenceBadge preference={application.preference} />
         </div>
