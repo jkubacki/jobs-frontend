ffcf7dc90e51760b0bcdc124138afa84b4c751d6
Jakub Kubacki <kubacki.jk@gmail.com>
2024-05-18 15:26:06 +0200

    Use badge with number of applications

--- components/Applications/ApplicationsList/ApplicationsList.tsx
+++ components/Applications/ApplicationsList/ApplicationsList.tsx
@@ -1,14 +1,12 @@
-import { SendHorizonal } from 'lucide-react'
-
 import { ApplicationCard } from '@/components/Applications/ApplicationsList/ApplicationCard'
 import { Application } from '@/lib/applications/types/Application'
+import { Badge } from '@/components/ui/badge'
 
 export function ApplicationsList({ applications }: { applications: Application[] }) {
   return (
     <div className="w-full flex flex-col gap-3">
       <div className="text-sm text-muted-foreground flex items-center gap-1">
-        Applications
-        <SendHorizonal className="h-4 w-4" />
+        <Badge variant={'outline'}>{applications.length} applications</Badge>
       </div>
       {applications.map(application => (
         <ApplicationCard key={application.id} application={application} />
