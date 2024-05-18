837be88694daa920a5e6209fc386fd2038209688
Jakub Kubacki <kubacki.jk@gmail.com>
2024-05-18 15:58:55 +0200

    Cover letter badge with dialog

--- components/Applications/ApplicationsList/ApplicationCard.tsx
+++ components/Applications/ApplicationsList/ApplicationCard.tsx
@@ -3,21 +3,17 @@ import { Card } from '@/components/ui/card'
 import { BadgeColors } from '@/components/Listings/ListingCard/Badges/BadgeColors'
 import { PreferenceBadge } from '@/components/Listings/ListingCard/Badges/PreferenceBadge'
 import { ListingBadge } from '@/components/Listings/ListingCard/Badges/ListingBadge'
-import { CoverLetter } from '@/components/Applications/ApplicationsList/CoverLetter'
+import { CoverLetterBadge } from '@/components/Applications/ApplicationsList/ApplicationCard/Badges/CoverLetterBadge'
 
 export function ApplicationCard({ application }: { application: Application }) {
   const cvColor = application.cv ? 'green-600' : BadgeColors.negative
   return (
     <Card className="flex flex-col items-center gap-2 font-normal p-3 text-sm" title="Application">
-      <div className="flex gap-2 justify-between w-full">
+      <div className="flex gap-1 justify-between w-full">
         <ListingBadge title="Applied at">{application.applied_at}</ListingBadge>
-        <div className="flex gap-2">
-          <ListingBadge
-            title="CV sent?"
-            color={application.cv ? BadgeColors.positive : BadgeColors.neutral}
-          >
-            {application.cv ? 'CV sent' : 'CV not sent'}
-          </ListingBadge>
+        <div className="flex gap-1">
+          {application.cv && <ListingBadge title="CV sent">CV</ListingBadge>}
+          <CoverLetterBadge cover_letter={application.cover_letter} />
           <PreferenceBadge preference={application.preference} />
         </div>
       </div>
