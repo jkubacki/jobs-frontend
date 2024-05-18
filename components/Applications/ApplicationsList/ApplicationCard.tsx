56b5bc1d9a961117e0f858fdf3b42ddea4276974
Jakub Kubacki <kubacki.jk@gmail.com>
2024-05-18 15:59:05 +0200

    Update ApplicationCard.tsx

--- components/Applications/ApplicationsList/ApplicationCard.tsx
+++ components/Applications/ApplicationsList/ApplicationCard.tsx
@@ -1,12 +1,10 @@
 import { Application } from '@/lib/applications/types/Application'
 import { Card } from '@/components/ui/card'
-import { BadgeColors } from '@/components/Listings/ListingCard/Badges/BadgeColors'
 import { PreferenceBadge } from '@/components/Listings/ListingCard/Badges/PreferenceBadge'
 import { ListingBadge } from '@/components/Listings/ListingCard/Badges/ListingBadge'
 import { CoverLetterBadge } from '@/components/Applications/ApplicationsList/ApplicationCard/Badges/CoverLetterBadge'
 
 export function ApplicationCard({ application }: { application: Application }) {
-  const cvColor = application.cv ? 'green-600' : BadgeColors.negative
   return (
     <Card className="flex flex-col items-center gap-2 font-normal p-3 text-sm" title="Application">
       <div className="flex gap-1 justify-between w-full">
