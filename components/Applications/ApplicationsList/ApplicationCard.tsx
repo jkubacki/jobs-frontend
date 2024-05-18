394442793f91c486c3b79ffe1ca5a2cdac131770
Jakub Kubacki <kubacki.jk@gmail.com>
2024-05-18 16:13:09 +0200

    Add random number of replies

--- components/Applications/ApplicationsList/ApplicationCard.tsx
+++ components/Applications/ApplicationsList/ApplicationCard.tsx
@@ -5,17 +5,23 @@ import { ListingBadge } from '@/components/Listings/ListingCard/Badges/ListingBa
 import { CoverLetterBadge } from '@/components/Applications/ApplicationsList/ApplicationCard/Badges/CoverLetterBadge'
 
 export function ApplicationCard({ application }: { application: Application }) {
+  const repiesCount = Math.floor(Math.random() * 10)
+  const repliesWord = repiesCount === 1 ? 'reply' : 'replies'
+
   return (
-    <Card className="flex flex-col items-center gap-2 font-normal p-3 text-sm" title="Application">
+    <Card className="flex flex-col items-center gap-4 font-normal p-3 text-sm" title="Application">
+      <div className="text-sm">Applied at {application.applied_at}</div>
+      <div className="text-muted-foreground">{application.notes}</div>
       <div className="flex gap-1 justify-between w-full">
-        <ListingBadge title="Applied at">{application.applied_at}</ListingBadge>
         <div className="flex gap-1">
           {application.cv && <ListingBadge title="CV sent">CV</ListingBadge>}
           <CoverLetterBadge cover_letter={application.cover_letter} />
           <PreferenceBadge preference={application.preference} />
         </div>
+        <ListingBadge title="Applied at">
+          {repiesCount === 0 ? 'no' : repiesCount} {repliesWord}
+        </ListingBadge>
       </div>
-      <div className="text-muted-foreground">{application.notes}</div>
     </Card>
   )
 }
--- components/Applications/ApplicationsList/ApplicationsList.tsx
+++ components/Applications/ApplicationsList/ApplicationsList.tsx
@@ -3,10 +3,14 @@ import { Application } from '@/lib/applications/types/Application'
 import { Badge } from '@/components/ui/badge'
 
 export function ApplicationsList({ applications }: { applications: Application[] }) {
+  const applicationsWord = applications.length === 1 ? 'application' : 'applications'
+
   return (
     <div className="w-full flex flex-col gap-3">
       <div className="text-sm text-muted-foreground flex items-center gap-1">
-        <Badge variant={'outline'}>{applications.length} applications</Badge>
+        <Badge variant={'outline'}>
+          {applications.length} {applicationsWord}
+        </Badge>
       </div>
       {applications.map(application => (
         <ApplicationCard key={application.id} application={application} />
