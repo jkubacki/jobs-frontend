f4f285fe06f62834fa5a171c5569e0ce965e6a55
Jakub Kubacki <kubacki.jk@gmail.com>
2024-05-18 14:24:44 +0200

    ApplicationForm

--- components/Listings/Dialogs/CreateApplicationDialog.tsx
+++ components/Listings/Dialogs/CreateApplicationDialog.tsx
@@ -3,16 +3,19 @@
 import { z } from 'zod'
 
 import { Dialog, DialogContent, DialogTitle, DialogDescription } from '@/components/ui/dialog'
-import { ListingForm, listingFormSchema } from '@/components/Listings/ListingForm/ListingForm'
 import { DialogHeader } from '@/components/ui/dialog'
 import { useAppDispatch, useAppSelector } from '@/lib/hooks'
 import { ApplicationsSelectors } from '@/lib/applications/ApplicationsSelectors'
 import { ApplicationsActions } from '@/lib/applications/applicationsSlice'
+import {
+  ApplicationForm,
+  applicationFormSchema,
+} from '@/components/Listings/ApplicationForm/ApplicationForm'
 
 export function CreateApplicationDialog() {
   const creatingFor = useAppSelector(ApplicationsSelectors.creatingFor)
-  const updating = useAppSelector(ApplicationsSelectors.updating)
-  const updatingError = useAppSelector(ApplicationsSelectors.updatingError)
+  const creating = useAppSelector(ApplicationsSelectors.creating)
+  const creatingError = useAppSelector(ApplicationsSelectors.creatingError)
 
   const dispatch = useAppDispatch()
 
@@ -25,16 +28,16 @@ export function CreateApplicationDialog() {
     >
       <DialogContent className="overflow-y-scroll max-h-[95%] sm:max-w-screen-xs md:max-w-screen-sm lg:max-w-screen-md">
         <DialogHeader>
-          <DialogTitle>Edit Listing</DialogTitle>
+          <DialogTitle>Add Application</DialogTitle>
           <DialogDescription></DialogDescription>
         </DialogHeader>
-        <ListingForm
-          onSubmit={(data: z.infer<typeof listingFormSchema>) => {
+        <ApplicationForm
+          onSubmit={(data: z.infer<typeof applicationFormSchema>) => {
             if (creatingFor) dispatch(ApplicationsActions.create({ data, listing: creatingFor }))
           }}
-          listing={creatingFor}
-          saving={updating}
-          error={updatingError}
+          application={null}
+          saving={creating}
+          error={creatingError}
         />
       </DialogContent>
     </Dialog>
--- lib/applications/types/Application.ts
+++ lib/applications/types/Application.ts
@@ -3,7 +3,7 @@ export interface Application {
   listing_id: number
   applied_at: string
   cv: boolean
-  cover_letter: boolean | null
+  cover_letter: string | null
   notes: string | null
   preference: number
 }
