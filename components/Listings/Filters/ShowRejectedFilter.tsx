091a14624b69aaf4f179d70b013e5e69f4a2a10b
Jakub Kubacki <kubacki.jk@gmail.com>
2024-05-28 14:01:06 +0200

    ShowRejectedFilter radio

---
+++ components/Listings/Filters/ShowRejectedFilter.tsx
@@ -0,0 +1,25 @@
+'use client'
+
+import { RadioTabs } from '@/components/ui/RadioTabs'
+import { useAppDispatch, useAppSelector } from '@/lib/hooks'
+import { ListingsSelectors } from '@/lib/listings/ListingsSelectors'
+import { ListingsState, ListingsActions } from '@/lib/listings/listingsSlice'
+
+const tabs: {
+  name: string
+  value: ListingsState['showRejected']
+}[] = [
+  { name: 'Not rejected', value: false },
+  { name: 'All', value: true },
+]
+
+export function ShowRejectedFilter() {
+  const dispatch = useAppDispatch()
+  const showRejected = useAppSelector(ListingsSelectors.showRejected)
+
+  const tabClick = (showRejected: ListingsState['showRejected'] | null) => {
+    if (showRejected !== null) dispatch(ListingsActions.setShowRejected({ showRejected }))
+  }
+
+  return <RadioTabs tabs={tabs} tabClick={tabClick} selected={showRejected} />
+}
--- components/Listings/ListingsSection.tsx
+++ components/Listings/ListingsSection.tsx
@@ -8,6 +8,7 @@ import { ListingsList } from '@/components/Listings/ListingsList/ListingsList'
 import { CreateApplicationDialog } from '@/components/Applications/Dialogs/CreateApplicationDialog'
 import { EditApplicationDialog } from '@/components/Applications/Dialogs/EditApplicationDialog'
 import { CreateReplyDialog } from '@/components/Replies/Dialogs/CreateReplyDialog'
+import { ShowRejectedFilter } from '@/components/Listings/Filters/ShowRejectedFilter'
 
 export function ListingsSection() {
   return (
@@ -20,7 +21,11 @@ export function ListingsSection() {
       </NavigationHeader>
       <main className="grid flex-1 items-start gap-4 p-4 sm:px-6 sm:py-0 md:gap-8">
         <div className="flex items-center">
-          <RemoteRadioFilter />
+          <div className="flex gap-2">
+            <RemoteRadioFilter />
+            <ShowRejectedFilter />
+          </div>
+
           <div className="ml-auto flex items-center gap-2">
             <CreateListingDialog />
             <CreateApplicationDialog />
