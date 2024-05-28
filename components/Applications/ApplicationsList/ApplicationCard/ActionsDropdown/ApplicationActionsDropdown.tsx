48c9250c9b45b1a10b639719b37e55e7925c1b55
Jakub Kubacki <kubacki.jk@gmail.com>
2024-05-28 12:22:42 +0200

    Rename ReplyToDropdownAction -> CreateReplyDropdownAction

--- components/Applications/ApplicationsList/ApplicationCard/ActionsDropdown/ApplicationActionsDropdown.tsx
+++ components/Applications/ApplicationsList/ApplicationCard/ActionsDropdown/ApplicationActionsDropdown.tsx
@@ -10,7 +10,7 @@ import {
 import { DeleteApplicationDropdownAction } from '@/components/Applications/ApplicationsList/ApplicationCard/ActionsDropdown/DropdownActions/DeleteApplicationDropdownAction'
 import { EditApplicationDropdownAction } from '@/components/Applications/ApplicationsList/ApplicationCard/ActionsDropdown/DropdownActions/EditApplicationDropdownAction'
 import { Application } from '@/lib/applications/types/Application'
-import { ReplyToDropdownAction } from '@/components/Applications/ApplicationsList/ApplicationCard/ActionsDropdown/DropdownActions/ReplyToDropdownAction'
+import { CreateReplyDropdownAction } from '@/components/Applications/ApplicationsList/ApplicationCard/ActionsDropdown/DropdownActions/CreateReplyDropdownAction'
 
 export function ApplicationActionsDropdown({ application }: { application: Application }) {
   return (
@@ -23,7 +23,7 @@ export function ApplicationActionsDropdown({ application }: { application: Appli
       </DropdownMenuTrigger>
       <DropdownMenuContent align="end">
         <DropdownMenuLabel>Actions</DropdownMenuLabel>
-        <ReplyToDropdownAction application={application} />
+        <CreateReplyDropdownAction application={application} />
         <EditApplicationDropdownAction application={application} />
         <DeleteApplicationDropdownAction application={application} />
       </DropdownMenuContent>
--- components/Applications/ApplicationsList/ApplicationCard/ActionsDropdown/DropdownActions/CreateReplyDropdownAction.tsx
+++ components/Applications/ApplicationsList/ApplicationCard/ActionsDropdown/DropdownActions/CreateReplyDropdownAction.tsx
@@ -3,12 +3,12 @@ import { DropdownMenuItem } from '@/components/ui/dropdown-menu'
 import { Application } from '@/lib/applications/types/Application'
 import { RepliesActions } from '@/lib/replies/repliesSlice'
 
-export function ReplyToDropdownAction({ application }: { application: Application }) {
+export function CreateReplyDropdownAction({ application }: { application: Application }) {
   const dispatch = useAppDispatch()
 
   const handleClick = () => {
     dispatch(RepliesActions.setReplyingTo({ application }))
   }
 
-  return <DropdownMenuItem onClick={handleClick}>Reply to</DropdownMenuItem>
+  return <DropdownMenuItem onClick={handleClick}>Add reply</DropdownMenuItem>
 }
