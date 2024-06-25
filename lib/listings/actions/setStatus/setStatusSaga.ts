929466e9c1b555e4dd324ad562189c06596d6348
Jakub Kubacki <kubacki.jk@gmail.com>
2024-06-25 12:06:36 +0200

    Change showRejected filter to status in store

--- lib/listings/actions/setStatus/setStatusSaga.ts
+++ lib/listings/actions/setStatus/setStatusSaga.ts
@@ -2,6 +2,6 @@ import { put } from 'typed-redux-saga'
 
 import { ListingsActions } from '@/lib/listings/listingsSlice'
 
-export function* setShowRejectedSaga(_action: ReturnType<typeof ListingsActions.setShowRejected>) {
+export function* setStatusSaga(_action: ReturnType<typeof ListingsActions.setStatus>) {
   yield* put(ListingsActions.load({ page: 1 }))
 }
